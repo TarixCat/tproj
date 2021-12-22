2f772b8bd27df68f422317154e5c3afc71c02b24
TarixCat <mrcatter05@gmail.com>
2021-12-22 17:20:59 +1100

    Added Command Provider to start up

--- src/main/java/me/devtarix/tproj/Main.java
+++ src/main/java/me/devtarix/tproj/Main.java
@@ -1,12 +1,14 @@
 package me.devtarix.tproj;
 
 import me.devtarix.tproj.cmdutils.CommandInterpreter;
+import me.devtarix.tproj.cmdutils.CommandProvider;
 import me.devtarix.tproj.extensions.ExtensionsProvider;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lo");
         new ExtensionsProvider();
+        new CommandProvider();
         ExtensionRegistry.ext(args);
         CommandInterpreter.getInstance();
     }
