72642524b22f55fb998785d6a3c50114938fcd1c
TarixCat <mrcatter05@gmail.com>
2022-05-26 22:29:33 +1000

    GuiMode isn't even a class dummy...

--- src/main/java/me/devtarix/tproj/extensions/ExtensionsProvider.java
+++ src/main/java/me/devtarix/tproj/extensions/ExtensionsProvider.java
@@ -2,7 +2,6 @@ package me.devtarix.tproj.extensions;
 
 public class ExtensionsProvider {
     public ExtensionsProvider() {
-        new TestExt();
-        new GuiMode();
+            new TestExt();
     }
 }
