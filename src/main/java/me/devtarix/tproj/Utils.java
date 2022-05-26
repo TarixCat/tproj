96d252f50a757dc5ef4e41d3b965b2bbca804120
TarixCat <mrcatter05@gmail.com>
2022-05-26 22:28:18 +1000

    Directory generation not yet required

--- src/main/java/me/devtarix/tproj/Utils.java
+++ src/main/java/me/devtarix/tproj/Utils.java
@@ -1,19 +1,6 @@
 package me.devtarix.tproj;
 
-import java.io.File;
-
 public class Utils {
-    static String currentDirectory = System.getProperty("user.dir");
-    static String loc = currentDirectory + "/randomproject";
-
-    public static String outdir = loc + "/out";
-    public static String keydir = loc + "/keys";
-    public static String comdir = loc + "/data";
-    public static String rsadir = keydir + "/rsa";
-    public static String aesdir = keydir + "/aes";
-
-    File file = new File(loc);
-
     public static void log(String msg) {
         System.out.println(msg);
     }
