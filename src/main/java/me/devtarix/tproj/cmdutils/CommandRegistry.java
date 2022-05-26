b82446dbe08abfbac520dae7e1c05147bd29f0c2
TarixCat <mrcatter05@gmail.com>
2022-05-26 22:25:50 +1000

    Preparation to implement help command

--- src/main/java/me/devtarix/tproj/cmdutils/CommandRegistry.java
+++ src/main/java/me/devtarix/tproj/cmdutils/CommandRegistry.java
@@ -1,7 +1,17 @@
 package me.devtarix.tproj.cmdutils;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CommandRegistry {
-    public static HashMap<String, Commands> registry = new HashMap<>();
+    public static HashMap<String, Command> registry = new HashMap<>();
+
+    public void loadCommands() {
+        for(Map.Entry<String, Command> entry : registry.entrySet()) {
+            String key = entry.getKey();
+            Command value = entry.getValue();
+
+            value.register(key);
+        }
+    }
 }
