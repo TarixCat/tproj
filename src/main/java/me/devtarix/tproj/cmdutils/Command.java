5be6914ebf0f92636c514f708a9633af9738c74f
TarixCat <mrcatter05@gmail.com>
2022-05-26 22:24:27 +1000

    Minor Refactor

--- src/main/java/me/devtarix/tproj/cmdutils/Command.java
+++ src/main/java/me/devtarix/tproj/cmdutils/Command.java
@@ -2,7 +2,7 @@ package me.devtarix.tproj.cmdutils;
 
 import static me.devtarix.tproj.Utils.log;
 
-public interface Commands {
+public interface Command {
     default void register(String name) {
         CommandRegistry.registry.put(name, this);
         log("Registered command: " + name);
