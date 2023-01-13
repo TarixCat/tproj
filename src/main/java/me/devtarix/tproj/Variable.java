92effc5520dffbe11b9f29633666203cba7b3fc5
TarixCat <mrcatter05@gmail.com>
2023-01-13 16:40:45 +1100

    Created Variable.java

---
+++ src/main/java/me/devtarix/tproj/Variable.java
@@ -0,0 +1,15 @@
+package me.devtarix.tproj;
+
+public class Variable {
+
+    private int value;
+
+    Variable(String name) {
+
+    }
+
+    int Variable(String name, int value) {
+        this.value = value;
+        return this.value;
+    }
+}
